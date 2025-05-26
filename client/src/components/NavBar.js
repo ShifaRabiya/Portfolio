aed4eef99fe1b8714425d532f9c06dabe214ed1b
Shifa <shifarabiya4216@gmail.com>
2025-05-27 02:49:40 +0530

    Frontend edits

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -31,14 +31,6 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
         <Container>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <span className="navbar-text">
-                <div className="social-icon">
-                    <a href="https://www.linkedin.com/in/shifa-rabiya-4871b2255" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Linkedin"/></a>
-                    <a href="https://github.com/ShifaRabiya" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="GitHub"/></a>
-                </div>
-                <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span>
-                </button>
-            </span>
             <Navbar.Collapse id="basic-navbar-nav" className="mobile-menu">
             <Nav className="me-auto">
                 <Nav.Link
@@ -65,6 +57,14 @@ export const NavBar = () => {
             </Nav>
             
             </Navbar.Collapse>
+            <span className="navbar-text">
+                <div className="social-icon">
+                    <a href="https://www.linkedin.com/in/shifa-rabiya-4871b2255" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Linkedin"/></a>
+                    <a href="https://github.com/ShifaRabiya" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="GitHub"/></a>
+                </div>
+                <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span>
+                </button>
+            </span>
         </Container>
         </Navbar>
     )

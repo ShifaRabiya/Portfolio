1137b50dd3b2f1d0166eec19d01e69b6dc6d3b15
Shifa <shifarabiya4216@gmail.com>
2025-05-26 23:14:23 +0530

    Frontend edits

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -46,7 +46,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                     <span className="tagline">Welcome to my Portfolio</span>
-                    <h1>{`Hi! I'm Shifa `}<span className="wrap">{text}</span></h1>
+                    <h1>{`Hi I'm Shifa `}<span className="wrap">{text}</span></h1>
                     <p>About Yourself</p>
                     </Col >
                     <Col xs={12} md={6} xl={5}>

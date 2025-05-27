f807d9da2cc21bbed7c00695f556fb393d0a511e
Shifa <shifarabiya4216@gmail.com>
2025-05-27 14:45:53 +0530

    Frontend edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {NavBar} from './components/NavBar';
 import {Banner} from './components/Banner';
+import { About } from './components/About';
 import {Skills} from './components/Skills';
 import {Projects} from './components/Projects';
 import {ProjectCards} from './components/ProjectCards';
 import { Contact } from './components/Contact';
-import { Server } from 'react-bootstrap-icons';
+import { About } from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <About />
       <Skills />
       <Projects />
       <ProjectCards />

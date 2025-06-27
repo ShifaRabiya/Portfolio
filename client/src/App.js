49403840ce6d4965d17cdf7fc34867906a981bd0
Shifa <shifarabiya4216@gmail.com>
2025-06-27 14:29:08 +0530

    new project

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import {NavBar} from './components/NavBar';
 import {Banner} from './components/Banner';
 import {Skills} from './components/Skills';
 import {Projects} from './components/Projects';
-import {ProjectCards} from './components/ProjectCards';
 import { Contact } from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,7 +13,6 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <ProjectCards />
       <Contact />
     </div>
   );

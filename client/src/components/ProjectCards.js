49a70f905753de86998091fdd98c549979814e6a
Shifa04 <shifarabiya4216@gmail.com>
2025-11-11 01:00:56 +0530

    style: minor style changes

--- client/src/components/ProjectCards.js
+++ client/src/components/ProjectCards.js
@@ -1,15 +1,30 @@
-import { Col } from "react-bootstrap"
+import { Col } from "react-bootstrap";
 
-    export const ProjectCards = ({title, description, imgUrl}) => {
-    return (
-        <Col sm={6} md={4}>
-            <div className="proj-imgbx">
-                <img src={imgUrl} />
-                <div className="proj-txtx">
-                    <h4>{title}</h4>
-                    <span>{description}</span>
-                </div>
-            </div>
-        </Col>
-    );
+export const ProjectCards = ({ title, description, imgUrl, url }) => {
+  const CardContent = (
+    <div className="proj-imgbx">
+      <img src={imgUrl} alt={title} />
+      <div className="proj-txtx">
+        <h4>{title}</h4>
+        <span>{description}</span>
+      </div>
+    </div>
+  );
+
+  return (
+    <Col xs={12} md={4} className="project-card">
+      {url ? (
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          {CardContent}
+        </a>
+      ) : (
+        CardContent
+      )}
+    </Col>
+  );
 };
